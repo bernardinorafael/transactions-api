0ce8a6ffc9332705cf490bcd7827f7713ae1ad38
Rafael <rafaelferreirab2@gmail.com>
2023-02-18 20:35:09 -0300

    build: preparing app to build

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,11 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['production', 'development', 'test']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string({ required_error: 'DATABASE_URL not provided.' }),
-  PORT: z.number({ required_error: 'Server PORT not provided.' }).default(3333),
+  PORT: z.coerce
+    .number({ required_error: 'Server PORT not provided.' })
+    .default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
